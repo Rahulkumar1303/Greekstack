586bc4177c888a3f9f35355011dfb9738b9d6baf
Rahul Kumar <rkrahul1303@gmial.com>
2025-05-23 16:46:30 +0530

    sun-down with the help of animation

--- practise1/project-1/sundownproject/sundown.js
+++ practise1/project-1/sundownproject/sundown.js
@@ -2,7 +2,8 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('#main'),
     smooth: true
 });
-var elemc=document.querySelector('#elem-container')
+function page4Animation(){
+    var elemc=document.querySelector('#elem-container')
 var fixed=document.querySelector('#fixed-image')
 elemc.addEventListener("mouseenter",function(){
     fixed.style.display="block"
@@ -17,3 +18,37 @@ elems.forEach(function(e){
         fixed.style.backgroundImage=`url(${image})`
     })
 })
+}
+function swiperAnimation(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+      });
+}
+function menuAnimation(){
+    var menu=document.querySelector('nav h3')
+    var full=document.querySelector('#full-scr')
+    var navimg=document.querySelector("nav img")
+    var flag= 0
+    menu.addEventListener("click",function(){
+        if(flag==0){
+        full.style.top=0
+        navimg.style.opacity=0
+        flag=1
+       }
+       else{
+        full.style.top="-100%"
+        navimg.style.opacity=1
+        flag=0
+        }
+})
+}
+swiperAnimation()
+page4Animation()
+menuAnimation()
+
+var loader=document.querySelector("#loader")
+setTimeout(function(){
+    loader.style.top="-100%"
+},4200)
